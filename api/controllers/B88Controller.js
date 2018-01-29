2453f249f409dda0ae195fda3be0b3c17ed89737
giangvo1125 <giangvo1125@gmail.com>
2018-01-29 16:28:28 +0700

    uudpate

--- api/controllers/B88Controller.js
+++ api/controllers/B88Controller.js
@@ -78,7 +78,7 @@ module.exports = {
                                                     //nếu thoả điều kiện
                                                     if(condition1 && condition2 && condition3) {
                                                         stake = list_stake[0]
-                                                        var ticketResult = histories[0].result == 'Under' ? 'Over' : 'Under'
+                                                        var ticketResult = histories[0].result == 'Under' ? 'Tài' : 'Xỉu'
                                                         //bet
                                                         console.log('bet theo -----1------ ',ticketResult)
                                                         if(user.usertype == 0 || user.usertype == '0') {
@@ -118,8 +118,8 @@ module.exports = {
                                                         // user.loseticket++;
                                                         stake = list_stake[user.loseticket]
                                                         var stake_sub = subAmount// tiền bet vé dằn
-                                                        var ticketResult = histories[0].result == 'Under' ? 'Over' : 'Under'
-                                                        var ticketResult2 = ticketResult == 'Under' ? 'Over' : 'Under'
+                                                        var ticketResult = histories[0].result == 'Under' ? 'Tài' : 'Xỉu'
+                                                        var ticketResult2 = ticketResult == 'Under' ? 'Tài' : 'Xỉu'
                                                         //bet
                                                         var promise_bet = []
                                                         //vé bet 
